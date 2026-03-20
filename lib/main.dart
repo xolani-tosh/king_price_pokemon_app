66f0c3ec32047be366f76bf4851a0655d996f1ae
Xolani <xr.mkhwanazi.20@gmail.com>
2026-03-20 19:27:52 +0200

    Add firebase to main and update Color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:king_price_pokemon_app/views/registration/registration_page.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -12,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+ colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const RegistrationPage(title: 'Flutter Demo Home Page'),
     );
